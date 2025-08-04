bf161255c4ff424fdc24ae1b4de95e93825f156e
Jesus-Iniesta <iniestavalverdejesus4@gmail.com>
2025-08-03 21:54:34 -0600

    feat: Migraciones de modelos inicial y personalizado

---
+++ Checks/urls.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+app_name = 'Checks'
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('home/', views.home, name='home'),
+    path('logout/', LogoutView.as_view(next_page='Checks:login'), name='logout'),
+]
